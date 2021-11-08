3def000c46d3c28a91c7693b540188df47d68f17
ATunaMelt <colekuntzman@protonmail.com>
2021-11-08 10:01:48 -0500

    Feat: Adding moralis and metamask integration

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 import './styles/App.scss';
 import Header from './components/header.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Main } from './components/Main.js';
 
 function App() {
   return (
     <Router>
-    <div className='App'>
-      <Header />
-      <div className='background' />
-      <Switch>
-        <Route path='/pool/new'>
-          <div>New Pool </div>
-        </Route>
-        <Route path='/pool/:id'>
-          <div>Join Pool </div>
-        </Route>
-        <Route path='/brackets/new'>
-          <div>New Bracket </div>
-        </Route>
-        <Route path='/brackets/:id'>
-          <div> View Bracket </div>
-        </Route>
-        <Route path='/about'>
-          <div> About </div>
-        </Route>
-        <Route path='/'>
-          <div> Home </div>
-        </Route>
-      </Switch>
-    </div>
+      <div className='App'>
+        <Header />
+        {/* <Main /> */}
+        <div className='background' />
+        <Switch>
+          <Route path='/pool/new'>
+            <div>New Pool </div>
+          </Route>
+          <Route path='/pool/:id'>
+            <div>Join Pool </div>
+          </Route>
+          <Route path='/brackets/new'>
+            <div>New Bracket </div>
+          </Route>
+          <Route path='/brackets/:id'>
+            <div> View Bracket </div>
+          </Route>
+          <Route path='/about'>
+            <div> About </div>
+          </Route>
+          <Route path='/'>
+            <div> Home </div>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
